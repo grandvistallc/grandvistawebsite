64ed6b6f24c3425c24f549a180c49d9d4168dd67
grandvistallc <mainline@grandvistadetailing.com>
2025-11-12 13:44:36 -0600

    Update Customer.js

--- views/Customer.js
+++ views/Customer.js
@@ -3240,7 +3240,7 @@ function showAddCustomerForm() {
 
 		// Add to CSV via server
 		try {
-			const response = await fetch('http://127.0.0.1:5000/add-customer', {
+			const response = await fetch('/add-customer', {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify(newCustomer)
